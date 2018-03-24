4327085e1da28240ac2545cda4dc4504784423ab
andreas_baggesgaard <andreas_baggesgaard@hotmail.com>
2018-03-24 15:28:52 +0100

    added DAL and made the first structure for a potential repository pattern

---
+++ Rawdataproj2/DataAccessLayer/Dto/Post/PostDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLayer.Dto.Post
+{
+    public class PostDto
+    {
+        public int Id { get; set; }
+        public int Score { get; set; }
+        public DateTime Creationdate { get; set; }
+        public string Body { get; set; }
+    }
+}
---
+++ Rawdataproj2/DataAccessLayer/IPostRepository.cs
@@ -0,0 +1,15 @@
+﻿using DataAccessLayer.Dto.Post;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLayer
+{
+    public interface IPostRepository : IDataRepository<PostDto, int>
+    {
+        // add post specific methods here - similar approach with the other entities
+
+        IList<PostDto> GetPostByName(string s);
+
+    }
+}
