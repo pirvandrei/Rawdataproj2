b6ae5d19dedbd27e76913fd9da67a18c0b791913
szymonmat <bosik111@gmail.com>
2018-05-16 11:17:48 +0200

    web setup

--- Rawdataproj2/WebService/Startup.cs
+++ Rawdataproj2/WebService/Startup.cs
@@ -32,7 +32,7 @@ namespace WebService
             }
 
             app.UseMvc();
-
+            app.UseFileServer();
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
---
+++ Rawdataproj2/WebService/wwwroot/js/main.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    console.log('test')
+
+    require.config({
+        baseUrl: 'bower_components',
+        paths: {
+            knockout: 'knockout/dist/knockout',
+            jquery: 'jQuery/dist/jquery.min',
+            text: 'requirejs-text/text',
+            jqcloud: 'jqcloud2/dist/jqcloud'
+        }
+    });
+})();
