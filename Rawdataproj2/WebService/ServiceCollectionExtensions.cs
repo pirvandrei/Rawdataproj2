aee5e7e4578b396315dbf0eb2a050ea8ca494236
andreas_baggesgaard <andreas_baggesgaard@hotmail.com>
2018-05-23 10:44:38 +0200

    Renamed repositories to contracts and added folder structure

--- Rawdataproj2/WebService/ServiceCollectionExtensions.cs
+++ Rawdataproj2/WebService/ServiceCollectionExtensions.cs
@@ -1,10 +1,6 @@
 ﻿using DataService;
 using Microsoft.Extensions.DependencyInjection;
 using StackoverflowContext;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace WebService
 {
