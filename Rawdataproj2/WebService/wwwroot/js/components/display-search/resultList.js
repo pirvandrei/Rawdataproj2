69afed969769bf1265140a2e1848e96f3395a958
szymonmat <bosik111@gmail.com>
2018-05-29 11:04:50 +0200

    fixed pagination

--- Rawdataproj2/WebService/wwwroot/js/components/display-search/resultList.js
+++ Rawdataproj2/WebService/wwwroot/js/components/display-search/resultList.js
@@ -1,28 +1,34 @@
-﻿define(['knockout', 'request','paginate'], function (ko, req,pg) {
+﻿
+define(['knockout', 'request','paginate'], function (ko, req,pg) {
     return function (params) {
         var searchString = ko.observable(params.searchString);
         var searchMethod = ko.observable(params.searchMethod);
         var startDate = ko.observable(params.startDate);
         var endDate = ko.observable(params.endDate);
-        var currentPage = ko.observableArray([1,2,3,4,5]);
-        var pData = ko.observable();
-        var changePage = ko.observable();
         var currentPagenr = ko.observable();
         var previousLink = ko.observable;
         var nextLink = ko.observable;
         var cPage = ko.observableArray();
-    
+
+        //Pagination button bindings
         var changePageNext = function () {
             if (pg.moveCursor('next')) {
-                cPage(pg.data.items)
-                console.log(pg.data.items)
+                
+                setTimeout(function () {
+                    cPage(pg.data.items)
+                    currentPagenr(pg.data.currentPage);
+                    window.scrollTo(0, 0);
+                },500);
             }
         }
+
         var changePagePrev = function () {
             if (pg.moveCursor('prev')) {
-                cPage(pg.data.items)
-                console.log(pg.data.items)
-
+                    setTimeout(function () {
+                    cPage(pg.data.items)
+                    currentPagenr(pg.data.currentPage);
+                    window.scrollTo(0,0);
+                }, 500);
             }
         }
         ko.computed(function () {
@@ -32,15 +38,12 @@
                 pg.loadData(data)
                 cPage(pg.data.items)
                 currentPagenr(pg.data.currentPage);
-                console.log(currentPagenr());
             });
         });
 
         
 
     return {
-        currentPage,
-        changePage,
         cPage,
         pg,
         currentPagenr,
